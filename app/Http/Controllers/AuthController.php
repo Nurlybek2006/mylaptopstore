9737ab803e8925257a65021ebf08cbcdc9eb0815
Nurlybek <zzzzzzzhhhhhhh693@gmail.com>
2025-11-29 19:02:31 +0500

    logout жасалды

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -86,10 +86,10 @@ class AuthController extends Controller
     public function logout(Request $request)
     {
         Auth::logout();
-
+        
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-
-        return redirect()->route('home');
+        
+        return redirect()->route('login')->with('success', 'Сіз жүйеден сәтті шықтыңыз!');
     }
 }
